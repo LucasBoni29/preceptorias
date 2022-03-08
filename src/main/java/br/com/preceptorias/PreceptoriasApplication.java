651118292d74ca052f7dfa001053493b38b08589
LucasBoni29 <santosboni2002@gmail.com>
2022-03-08 15:51:51 -0300

    Subindo o Projeto no Git

---
+++ src/main/java/br/com/preceptorias/PreceptoriasApplication.java
@@ -0,0 +1,13 @@
+package br.com.preceptorias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreceptoriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreceptoriasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/preceptorias/PreceptoriasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.preceptorias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreceptoriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
